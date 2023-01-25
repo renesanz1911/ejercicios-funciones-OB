9559be76a71142a3c4283c7ffbcf4d870b758d80
renesanz1911 <renesanz1911@gmail.com>
2023-01-25 19:26:59 -0300

    ejercicio suma

---
+++ suma_3_valores/src/Main.java
@@ -0,0 +1,11 @@
+
+
+public class Main {
+    public static void main (String[] args) {
+        int resultado = suma (6, 5,12 );
+        System.out.println(resultado);
+    }
+    public static int suma (int a,int b, int c){
+        return a + b + c;
+    }
+}
